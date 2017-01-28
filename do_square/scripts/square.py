204cdf28052eab2ac3e85235b36c634ccbab2d3c
dhash <dhash@Supernova-2ML-DE5450>
2017-01-27 20:54:53 -0500

    added untested square code

--- do_square/scripts/square.py
+++ do_square/scripts/square.py
@@ -12,33 +12,67 @@ class Neato:
         rospy.init_node('square')
         
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.rate = rospy.Rate(5)
+        self.distance = rospy.get_param("distance", 1)
         self.speed = rospy.get_param("speed", 1)
-        self.last_keypoint = self.log_position(rospy.wait_for_message('/odom', Odometry))
         
-    
-    def logic(self):
-        if phase == "Forward":
-            if self.log_position[0]
-
-
-    def turn_left(v):
-        return Twist(Vector3(0, 0, 0), Vector3(0, 0, v)) #TODO
-
-    def turn_right(v):
-        return Twist(Vector3(0, 0, 0), Vector3(0, 0, -v)) #TODO
-
-    def go_fwd(v):
-        return Twist(Vector3(v, 0, 0), Vector3(0, 0, 0))
-
-    def log_position(msg):
+        self.last_keypoint = self.curr_position(rospy.wait_for_message('/odom', Odometry))
+        self.curr_pos = self.last_keypoint
+        
+               
+        self.rate = rospy.Rate(5)
+     
+    def curr_position(msg):
         pos = [getattr(msg.position, coord) for coord in 'xyz']
         angle = [getattr(msg.position, coord) for coord in 'xyzw'] #TODO
-        return array([pos, angle])
+        self.curr_pos = array([pos, angle])
+        return self.curr_pos
+   
+    def logic(self):
+        distance, angle = self.curr_pos - self.last_keypoint 
+        if self.phase == "Forward":
+            if norm(distance) < self.distance:
+                self.pub.publish(go_fwd(self.speed))
+                rospy.loginfo("{} at {}".format(self.phase, self.speed))
+            if norm(distance) >= self.distance:
+                self.pub.publish(stop())
+                rospy.loginfo("Reached polygon edge, beginning to Rotate")
+                self.phase = "Rotate"
+                self.last_keypoint = self.curr_pos
+        if self.phase == "Rotate":
+            #TODO
+            if angle < (np.pi/2):
+                self.pub.publish(turn_left(self.speed))
+                rospy.loginfo("{} at {}".format(self.phase, self.speed))
+            #TODO
+            if angle >= (np.pi/2):
+                self.pub.publish(stop())
+                self.rospy.loginfo("Done rotating, beginning to move forward")
+                self.last_keypoint = self.curr_pos
+                self.phase = "Forward"
 
-    def drive_square(size):
+    def run(self):
         while not rospy.is_shutdown():
-            phase = self.phase
-            
+            logic()
+            self.rate.sleep()
+
+    @staticmethod
+    def stop():
+        return Twist(linear=Vector3(0, 0, 0),
+                     angular=Vector3(0, 0, 0))
+    @staticmethod
+    def turn_left(v):
+        return Twist(linear=Vector3(0, 0, 0),
+                     angular=Vector3(0, 0, v)) #TODO
+    
+    @staticmethod 
+    def turn_right(v):
+        return Twist(linear=Vector3(0, 0, 0),
+                     angular=Vector3(0, 0, -v)) #TODO
+    
+    @staticmethod 
+    def go_fwd(v):
+        return Twist(linear=Vector3(v, 0, 0), #TODO
+                     angular=Vector3(0, 0, 0))
 
 
+square = Neato()
